c78bd74c3d7d3315ff4cb496d3f3c3ed22872911
ahmed.mouzoune <ahmed.mouzoune@eemi.com>
2023-05-25 18:09:05 +0200

    test refacto deleteGame

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -317,35 +317,41 @@ class GameController extends AbstractController
    
         $currentUserId = $request->headers->get('X-User-Id');
 
-        if(ctype_digit($currentUserId) === true){
-            $player = $entityManager->getRepository(User::class)->find($currentUserId);
-
-            if($player !== null){
-
-                if(ctype_digit($id) === false){
-                    return new JsonResponse('Game not found', 404);
-                }
-        
-                $game = $entityManager->getRepository(Game::class)->findOneBy(['id' => $id, 'playerLeft' => $player]);
-
-                if(empty($game)){
-                    $game = $entityManager->getRepository(Game::class)->findOneBy(['id' => $id, 'playerRight' => $player]);
-                }
-
-                if(empty($game)){
-                    return new JsonResponse('Game not found', 403);
-                }
-
-                $entityManager->remove($game);
-                $entityManager->flush();
-
-                return new JsonResponse(null, 204);
-
-            }else{
-                return new JsonResponse('User not found', 401);
-            }
-        }else{
+        if(ctype_digit($currentUserId) === false){
             return new JsonResponse('User not found', 401);
         }
+        // if(ctype_digit($currentUserId) === true){
+        $player = $entityManager->getRepository(User::class)->find($currentUserId);
+
+        if($player === null){
+            return new JsonResponse('User not found', 401);
+        }
+        // if($player !== null){
+
+            if(ctype_digit($id) === false){
+                return new JsonResponse('Game not found', 404);
+            }
+    
+            $game = $entityManager->getRepository(Game::class)->findOneBy(['id' => $id, 'playerLeft' => $player]);
+
+            if(empty($game)){
+                $game = $entityManager->getRepository(Game::class)->findOneBy(['id' => $id, 'playerRight' => $player]);
+            }
+
+            if(empty($game)){
+                return new JsonResponse('Game not found', 403);
+            }
+
+            $entityManager->remove($game);
+            $entityManager->flush();
+
+            return new JsonResponse(null, 204);
+
+        // }else{
+        //     return new JsonResponse('User not found', 401);
+        // }
+        // }else{
+        //     return new JsonResponse('User not found', 401);
+        // }
     }
 }
