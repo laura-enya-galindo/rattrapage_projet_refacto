9ba50564bcc97374bfc601277966f8dc699b1c0a
Olive ADE <oliveade@MacBook-Air-de-Olive.local>
2024-05-29 13:32:56 +0200

    formtypegame

--- src/Forms/gameFormType.php
+++ src/Forms/gameFormType.php
@@ -6,16 +6,26 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class TaskType extends AbstractType
+class gameType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('task', TextType::class)
-            ->add('dueDate', DateType::class)
-            ->add('save', SubmitType::class)
-        ;
+        ->add('choice', TextType::class, [
+            'constraints' => [
+                new Assert\NotBlank()
+            ]
+        ])
+        ->getForm();
+
+    }
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => null, 
+        ]);
     }
 }
